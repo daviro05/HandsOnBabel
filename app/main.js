f20f6e5c24c2363e9439dd6845993a9a27eaf81b
Carlos Lopez <carlos.lmartinez@babel.es>
2018-02-28 12:22:22 +0100

    form About

--- app/main.js
+++ app/main.js
@@ -2,10 +2,18 @@ export class Main {
     constructor() {
         this.vista = {
             eBtnMenuMob: document.querySelector('#menuBtn'),
-            eMenuMob: document.querySelector('.menuMob ul')
+            eMenuMob: document.querySelector('.menuMob ul'),
+            eBtnSug: document.querySelector('#btnSug'),
+            eFormSug: document.querySelector('#formSug'),
+            eFormSugC: document.querySelector('#formSugC'),
+            eNameForm: document.querySelector('#nombre'),
+            eMailForm: document.querySelector('#correo'),
+            ePrefForm: document.querySelectorAll('#formSug input[type="checkbox"]'),
+            oPrefForm: [],
         }
-
         this.vista.eBtnMenuMob.addEventListener('click', this.desplegarMenu.bind(this), false);
+        this.vista.eBtnSug.addEventListener('click', this.desplegarFormSug.bind(this), false);
+        this.vista.eformSug ? this.vista.eformSug.addEventListener('submit', this.manejarForm.bind(this), false): null;
     }
 
     desplegarMenu() {
@@ -14,6 +22,28 @@ export class Main {
         } else {
             this.vista.eMenuMob.style.display = 'none';
         }
+    }
+
+    desplegarFormSug() {
+        if (!this.vista.eFormSugC.style.display || this.vista.eFormSugC.style.display==='none') {
+            this.vista.eFormSugC.style.display = 'block';
+        } else {
+            this.vista.eFormSugC.style.display = 'none';
+        }
+    }
+
+    manejarForm(oEv){
+        oEv.preventDefault();
+        localStorage.setItem('nombre',this.vista.eNameForm.value);
+        localStorage.setItem('correo',this.vista.eMailForm.value);
+        this.vista.ePrefForm.forEach(item => {
+            if(item.checked){
+                this.vista.oPrefForm.push(item.id);
+            }
+        });
+        let pref = JSON.stringify(this.vista.oPrefForm);
+        localStorage.setItem('preferencias', pref);
+        document.location.href="resultados.html";
     }
 
 }
---
+++ app/resultados.js
@@ -0,0 +1,18 @@
+(function () {
+    
+    function rellenaPref(){
+        let prefs = []
+        prefs = JSON.parse(localStorage.getItem('preferencias'));
+        let text = "";
+        prefs.forEach(item => {
+            text += `<li>${item}</li>`;
+        });
+        document.querySelector('#prefRes').innerHTML = text;
+    }
+
+    document.addEventListener("DOMContentLoaded", () => {
+        localStorage.getItem('nombre') ? document.querySelector('#nombreRes').innerHTML = localStorage.getItem('nombre') : null;
+        localStorage.getItem('correo') ? document.querySelector('#correoRes').innerHTML = localStorage.getItem('correo') : null;
+        localStorage.getItem('preferencias') ? rellenaPref() : null;
+    }, false)
+})()
