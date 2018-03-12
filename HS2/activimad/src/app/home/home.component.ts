85d95b0c97f67486d2a76280ea007c3cd623a563
daviro05 <daviro05@ucm.es>
2018-03-12 22:18:50 +0100

    Pruebas menu 2

--- HS2/activimad/src/app/home/home.component.ts
+++ HS2/activimad/src/app/home/home.component.ts
@@ -3,9 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   template: `
-    <p>
-     lorem
-    </p>
+    <div class="principal">
+    </div>
   `,
   styles: []
 })
