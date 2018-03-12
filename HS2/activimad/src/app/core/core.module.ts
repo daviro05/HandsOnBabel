5492d4bc246ab645407808fef35b852365c6e2e8
David Rodriguez <david.rmarco@babel.es>
2018-03-12 13:31:21 +0100

    RouterModule en core

--- HS2/activimad/src/app/core/core.module.ts
+++ HS2/activimad/src/app/core/core.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [MenuComponent, HeaderComponent, FooterComponent],
   declarations: [MenuComponent, HeaderComponent, FooterComponent]
