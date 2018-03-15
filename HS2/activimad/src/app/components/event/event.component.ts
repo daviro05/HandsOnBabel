6f91610e646bc7154560e46a3c59db751b5052e5
David Rodriguez <david.rmarco@babel.es>
2018-03-15 13:21:51 +0100

    Cambios en form, home, aside, sugerencias, etc

--- HS2/activimad/src/app/components/event/event.component.ts
+++ HS2/activimad/src/app/components/event/event.component.ts
@@ -8,10 +8,19 @@ import { Component, OnInit, Input } from '@angular/core';
 export class EventComponent implements OnInit {
   @Input() event: any;
   @Input() resumen: boolean;
+  ruta: string;
+  imagenes: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.imagenes = './assets/fotos/';
+    this.imgAleatoria();
+  }
+
+  imgAleatoria() {
+    let x = Math.floor((Math.random() * 13) + 1);
+    this.ruta = this.imagenes + x + '.jpg';
   }
 
 }
