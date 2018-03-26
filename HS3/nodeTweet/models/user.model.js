b6b5bbf19ee36ef3e10ca644f9ed73d6ecd657a8
CharlieGnomo <carlomaroficial@gmail.com>
2018-03-26 18:09:45 +0200

    user error + user get  - done (promesa se resuelve solo en ruta)

--- HS3/nodeTweet/models/user.model.js
+++ HS3/nodeTweet/models/user.model.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const APIError = require('../lib/apierror');
 
 const User = mongoose.model('User', { // El name corresponde con la coleccion.
   id: String,
@@ -12,14 +11,7 @@ const User = mongoose.model('User', { // El name corresponde con la coleccion.
 
 
 function getUser(id) {
-  return new Promise((resolve, reject) => {
-    let user = User.findOne({ id: id }).then((existUser) => {
-    if(existUser){
-      resolve(user);
-    }
-    reject(new APIError('El usuario no existe'));
-  });
-  });
+    return User.findOne({ id: id });
 }
 
 module.exports = {
--- HS3/nodeTweet/routes/users.js
+++ HS3/nodeTweet/routes/users.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+const APIError = require('../lib/apierror');
+
 const userController = require('../controllers/user.controller');
 
 /* Con esta ruta obtenemos la info de un usuario */
 router.route('/:id')
 .get(function(req, res, next) {
   return userController.getUser(req.params.id)
-  .then(res.json.bind(res))
-  .catch(next);
+  .then((data) => {
+    if(data){
+      return res.json(data);
+    }
+    next(new APIError("El usuario no existe"));
+  })
+  .catch(console.error);
 });
 
 
