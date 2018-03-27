1d0cb7be6a129f2b9f7555b6c2a3b9bec6cacba1
CharlieGnomo <carlomaroficial@gmail.com>
2018-03-27 20:34:11 +0200

    apitwitter comentado retweets

--- HS3/nodeTweet/workers/apitwitter.js
+++ HS3/nodeTweet/workers/apitwitter.js
@@ -22,29 +22,57 @@ const User = mongoose.model('UsersApi',
         statuses_count: Number,
     });
 
-
-const client = new Twitter({
+/* const client = new Twitter({
     consumer_key: "asRcZ4icN3zHbzr8eEzze0SrD",
     consumer_secret: "YbBLNvTuAwX21vG13qx8D80f2ioXsSnsiNbgC2Kwh3LjcVeAGw",
     access_token_key: "412317152-viVlZiZhlg54vZNeQSnLCUW6ZnVQIQThfdzbcA91",
     access_token_secret: "dQjeZVA9jZYleqrZiNe3fqnDLeakrtkz2BwV4tES2fjQG"
+}); */
+
+const client = new Twitter({
+    consumer_key: "qKRqYxvy5Tzk2M85mAL0akvr6",
+    consumer_secret: "JStIxsU9kfBNSffn6NIkQmbc2EwtpKsdpOy7u1EAmHZRoywt9s",
+    access_token_key: "2617008931-suJ9nVCdLMBHTomiDMWWZcbbtBDEZmse0w0ORWE",
+    access_token_secret: "CSwZgQnmuWnO4zhZHKilVVA0Z2NyYXFjOLgFmaZUNfPVb"
 });
 
 client.stream('statuses/filter', { track: 'hola', language: 'es' }, function (stream) {
     stream.on('data', function (tweet) {
         //console.log(tweet.user);
         var TwitterData = new Tweet(tweet); // create object 
-        TwitterData.save(); // save data to DB
+         // save data to DB
         //console.log(tweet);
+        /* let retweeters = [];
+        console.log('idTweet', tweet.id_str);
+        console.log(typeof tweet.id_str);
+        client.get('statuses/retweeters/ids', {id: tweet.id_str, count: 10, stringify_ids: 'true'})
+        .then(data => {
+            console.log(data.ids);
+        })
+        .catch(error => console.error(error)); */
+
+        /* TwitterData.users = retweeters; */
+        /* console.log(TwitterData.users); */
+        TwitterData.save(); 
         var UserData = new User(tweet.user); // create object 
         UserData.save(); // save data to DB
-
+         /* '971484200363798529' */ 
     });
 
     stream.on('error', function (error) {
         console.log(error);
     });
 });
+
+/* client.get('statuses/retweeters/ids', {id: '978685472800206849', cursor: 0, stringify_ids: 'true'})
+        .then(data => {
+            retweeters = data.ids;
+            console.log('retweeters',data.ids);
+        }) */
+
+
+
+
 
 /* client.stream('statuses/retweeters/ids', {  }, function (stream) {
     stream.on('data', function (tweet) {
