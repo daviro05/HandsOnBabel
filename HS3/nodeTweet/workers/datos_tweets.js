2dcbea9ae8512c0ebcc95e06cbf408efbb2783be
Carlos Lopez <carlos.lmartinez@babel.es>
2018-03-27 09:26:42 +0200

    data files.js

---
+++ HS3/nodeTweet/workers/datos_tweets.js
@@ -0,0 +1,71 @@
+module.exports = [
+    {
+        "id": "1",
+        "text": "Soy el tweet 1",
+        "user_id": "1",
+        "users": [
+            {
+                "_id": ObjectId("5ab9ef137229c80ec6474b13"),
+                "id": "1",
+                "name": "carlos",
+                "description": "hola soy carlos",
+                "url": "internet1",
+                "statuses_count": 2
+            }
+        ]
+    },
+    {
+        "id": "2",
+        "text": "Soy el tweet 2",
+        "user_id": "2",
+        "users": [
+            {
+                "_id": ObjectId("5ab9ef887229c80ec6474b19"),
+                "id": "2",
+                "name": "david",
+                "description": "hola soy david",
+                "url": "internet2",
+                "statuses_count": 4
+            },
+            {
+                "_id": ObjectId("5ab9ef137229c80ec6474b13"),
+                "id": "1",
+                "name": "carlos",
+                "description": "hola soy carlos",
+                "url": "internet1",
+                "statuses_count": 2
+            }
+        ]
+    },
+    {
+        "id": "3",
+        "text": "Soy el tweet 3",
+        "user_id": "3",
+        "users": [
+            {
+                "_id": ObjectId("5ab9efa97229c80ec6474b1c"),
+                "id": "3",
+                "name": "loren",
+                "description": "hola soy loren",
+                "url": "internet3",
+                "statuses_count": 6
+            },
+            {
+                "_id": ObjectId("5ab9ef887229c80ec6474b19"),
+                "id": "2",
+                "name": "david",
+                "description": "hola soy david",
+                "url": "internet2",
+                "statuses_count": 4
+            },
+            {
+                "_id": ObjectId("5ab9ef137229c80ec6474b13"),
+                "id": "1",
+                "name": "carlos",
+                "description": "hola soy carlos",
+                "url": "internet1",
+                "statuses_count": 2
+            }
+        ]
+    }
+]
---
+++ HS3/nodeTweet/workers/datos_users.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        "id" : "1",
+        "name" : "carlos",
+        "description" : "hola soy carlos",
+        "url" : "internet1",
+        "statuses_count" : 2
+    },
+    {
+        "id" : "2",
+        "name" : "david",
+        "description" : "hola soy david",
+        "url" : "internet2",
+        "statuses_count" : 4
+    },
+    {
+        "id" : "3",
+        "name" : "loren",
+        "description" : "hola soy loren",
+        "url" : "internet3",
+        "statuses_count" : 6
+    }
+]
